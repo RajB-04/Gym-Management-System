3807456f0904fe333f4221c2fbaefc5744150fd0
Yuvraj Bhosale <bhosaleyuvraj04@gmail.com>
2025-05-01 23:10:25 +0530

    added 1 more members in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,7 @@ VALUES
 -- Insert Members
 INSERT INTO members(name, age, plan, gym_goals, status, phoneno, email, address) 
 VALUES 
-('Yuvraj Singh', 24, 1, 1, 'true', '9876543210', 'yuvraj@example.com', 'Pune Maharashtra'),
+('Yuvraj Bhosale', 24, 1, 1, 'true', '9876543210', 'yuvraj@example.com', 'Pune Maharashtra'),
 ('Anjali Mehta', 29, 2, 2, 'true', '9123456780', 'anjali@example.com', 'Mumbai Maharashtra'),
-('Rohit Sharma', 34, 3, 3, 'false', '9012345678', 'rohit@example.com', 'Delhi India');
+('Rohit Sharma', 34, 3, 3, 'false', '9012345678', 'rohit@example.com', 'Delhi India'),
+('Ajay Dorale', 25, 1, 2, 'true', '9876543210', 'ajay@hotmail.com', 'Pune Maharashtra');
